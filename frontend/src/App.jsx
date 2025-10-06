c60c6b14fe4d44cc37ab6a8ec59cb03a15e28306
Rishabh <rawatrishi1121@gmail.com>
2025-10-06 15:24:41 +0530

    setup react router and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import './index.css'
+import Home from './components/Home'
+import CreateBooks from './components/CreateBooks'
+import DeleteBook from './components/DeleteBook'
+import EditBook from './components/EditBook'
+import Show from './components/Show'
 
 function App() {
   
   return (
    <div>
     <Routes>
-      <Route path='' element={""}  ></Route>
-      <Route path='' element={""}  ></Route>
-      <Route path='' element={""}  ></Route>
-      <Route path='' element={""}  ></Route>
-      <Route path='' element={""}  ></Route>
+      <Route path='/' element={<Home />}  ></Route>
+      <Route path='/create' element={<CreateBooks />}  ></Route>
+      <Route path='/delete' element={<DeleteBook />}  ></Route>
+      <Route path='/update' element={<EditBook />}  ></Route>
+      <Route path='/show' element={<Show />}  ></Route>
     </Routes>
    </div>
   )
